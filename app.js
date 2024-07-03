1374b30797eae67fb81f2a8d7cf73b139e7f6e7e
johnwinifred <winifredjohnchidera123@gmail.com>
2024-07-03 13:10:19 +0100

    Updated time and day

--- app.js
+++ app.js
@@ -11,3 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+function updateDayTime() {
+    const now = new Date();
+    const optionsDay = { 
+        weekday: 'long',   
+    };
+    const optionsTime = {
+        hour: 'numeric', 
+        minute: 'numeric', 
+        second: 'numeric',
+        hour12: true
+    };
+    const currentDay = now.toLocaleDateString('en-US', optionsDay);
+    const currentTime = now.toLocaleTimeString('en-US', optionsTime);
+    
+    document.querySelector('.current-day').innerText = currentDay;
+    document.querySelector('.current-time').innerText = currentTime;
+}
+updateDayTime();
+setInterval(updateDayTime, 1000);
+
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
